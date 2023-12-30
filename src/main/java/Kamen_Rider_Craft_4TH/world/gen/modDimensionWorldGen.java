1053f8d7c42589106b9a76170465e38cf7fbcf01
cheesynacho <cheesynacho@users.noreply.github.com>
2023-12-30 12:47:43 -0500

    Update modDimensionWorldGen.java
    Modified dimension ID

--- src/main/java/Kamen_Rider_Craft_4TH/world/gen/modDimensionWorldGen.java
+++ src/main/java/Kamen_Rider_Craft_4TH/world/gen/modDimensionWorldGen.java
@@ -14,20 +14,20 @@ import net.minecraftforge.fml.common.registry.VillagerRegistry.IVillageCreationH
 public class modDimensionWorldGen
 {  
     public static final String HELHEIM_NAME = "helheim";
-    public static final int HELHEIM_DIM_ID = DimensionManager.getNextFreeDimId();
+    public static final int HELHEIM_DIM_ID = 20;
     public static final DimensionType HELHEIM_DIM_TYPE = DimensionType.register(HELHEIM_NAME, "_"+HELHEIM_NAME, HELHEIM_DIM_ID, worldProviderHelheimForest.class, true);
     
 
     public static final String SANDSOFTIME_NAME = "sand_of_time";
-    public static final int SANDSOFTIME_DIM_ID = DimensionManager.getNextFreeDimId()+1;
+    public static final int SANDSOFTIME_DIM_ID = 21;
     public static final DimensionType SANDSOFTIME_DIM_TYPE = DimensionType.register(SANDSOFTIME_NAME, "_"+SANDSOFTIME_NAME, SANDSOFTIME_DIM_ID, worldProviderSandOfTime.class, true);
    
     public static final String NORTHERN_BASE_NAME = "northern_base";
-    public static final int NORTHERN_BASE_DIM_ID = DimensionManager.getNextFreeDimId()+2;
+    public static final int NORTHERN_BASE_DIM_ID = 22;
     public static final DimensionType NORTHERN_BASE_DIM_TYPE = DimensionType.register(NORTHERN_BASE_NAME, "_"+NORTHERN_BASE_NAME, NORTHERN_BASE_DIM_ID, worldProvider_northern_base.class, true);
     
     public static final String RABBIT_HUTCH_NAME = "rabbit_hutch";
-    public static final int RABBIT_HUTCH_DIM_ID = DimensionManager.getNextFreeDimId()+4;
+    public static final int RABBIT_HUTCH_DIM_ID = 23;
     public static final DimensionType RABBIT_HUTCH_DIM_TYPE = DimensionType.register(RABBIT_HUTCH_NAME, "_"+RABBIT_HUTCH_NAME, RABBIT_HUTCH_DIM_ID, worldProvider_rabbit_hutch.class, true);
   
     /**
